e18e2297c89c46ddc16f7a2ace832def74cb8ba4
NaguioMervina <mervynaguio@gmail.com>
2026-07-07 19:03:03 +0800

    Optimize portfolio images

--- resources/js/data/assets.ts
+++ resources/js/data/assets.ts
@@ -1,11 +1,11 @@
 export const portfolioImages = {
     profile: {
-        mervin: '/images/profile/mervin-naguio.jpg',
+        mervin: '/images/profile/mervin-naguio.webp',
     },
     projects: {
-        l1Ticketing: '/images/projects/l1-ticketing.png',
-        rmmcGensan: '/images/projects/rmmc-gensan.png',
-        rmmcMi: '/images/projects/rmmc-mi.png',
-        tokdash: '/images/projects/tokdash.png',
+        l1Ticketing: '/images/projects/l1-ticketing.webp',
+        rmmcGensan: '/images/projects/rmmc-gensan.webp',
+        rmmcMi: '/images/projects/rmmc-mi.webp',
+        tokdash: '/images/projects/tokdash.webp',
     },
 } as const;
