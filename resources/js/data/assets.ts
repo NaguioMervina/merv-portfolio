7a2b9c4a83f76029d407f18d54e0318f9e5e6713
NaguioMervina <mervynaguio@gmail.com>
2026-07-10 01:23:00 +0800

    Optimize project images to WebP format

--- resources/js/data/assets.ts
+++ resources/js/data/assets.ts
@@ -4,10 +4,10 @@ export const portfolioImages = {
     },
     projects: {
         l1Ticketing: '/images/projects/l1-ticketing.webp',
-        picklebook: '/images/projects/picklebook.png',
+        picklebook: '/images/projects/picklebook.webp',
         rmmcGensan: '/images/projects/rmmc-gensan.webp',
         rmmcMi: '/images/projects/rmmc-mi.webp',
         tokdash: '/images/projects/tokdash.webp',
-        wmsuCms: '/images/projects/wmsu-cms.png',
+        wmsuCms: '/images/projects/wmsu-cms.webp',
     },
 } as const;
